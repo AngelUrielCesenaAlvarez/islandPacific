1f6172ef16e9eef9f3d86e1479f69d2022bf42bc
Angel Uriel Ceseña Alvarez <al21760123@ite.edu.mx>
2024-05-30 09:15:34 -0700

    problemas con router en profile/3

--- next.config.js
+++ next.config.js
@@ -7,4 +7,13 @@ module.exports = {
     config.resolve.modules.push(path.resolve('./src'));
     return config;
   },
+  async redirects() {
+    return [
+      {
+        source: '/page/profile',
+        destination: '/', // Redirige a la página de inicio si se intenta prerenderizar
+        permanent: false,
+      },
+    ]
+  },
 };
