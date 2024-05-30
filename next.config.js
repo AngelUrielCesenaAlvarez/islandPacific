d1824607313a350baa783bcecc8aa0322be46d65
Angel Uriel Ceseña Alvarez <al21760123@ite.edu.mx>
2024-05-30 13:38:09 -0700

    arreglando form

--- next.config.js
+++ next.config.js
@@ -4,7 +4,12 @@ module.exports = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
   webpack: (config, options) => {
+    // Agregar la carpeta src al array de módulos
     config.resolve.modules.push(path.resolve('./src'));
+
+    // Configuración de alias
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+
     return config;
   },
   async redirects() {
