2d7b543c873418bcf3f4a67986bcbe2291e946b6
Angel Uriel Ceseña Alvarez <al21760123@ite.edu.mx>
2024-05-31 12:51:01 -0700

    mejora de diseño

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,9 @@ import { MdLocationPin } from "react-icons/md";
 const Footer = () => {
   return (
     <div className="bg-[#03045E] text-white">
-      <div className="flex justify-around items-center p-4 border-b border-gray-700">
-        <div className="w-1/3">
-          <p className="text-base">
+      <div className="flex flex-col md:flex-row justify-around items-center p-4 border-b border-gray-700">
+        <div className="w-full md:w-1/3 p-4">
+          <p className="text-sm md:text-base">
             Somos honrados con numerosos reconocimientos, que demuestra nuestro
             compromiso con la excelencia y calidad en nuestros productos. Además,
             contamos con certificaciones que respaldan nuestro compromiso con los
@@ -19,7 +19,7 @@ const Footer = () => {
         </div>
         <ol className="list-none space-y-2">
           <li className="flex items-center text-lg">
-            <FontAwesomeIcon icon={faCheck} className="text-green-600 mr-2"/>
+            <FontAwesomeIcon icon={faCheck} className="text-green-600 mr-2" />
             Cultura
           </li>
           <li className="flex items-center text-lg">
@@ -32,20 +32,36 @@ const Footer = () => {
           </li>
         </ol>
       </div>
-      <div className="flex justify-between items-center p-4">
-        <img src="https://islandpacific.com.mx/wp-content/uploads/2023/09/logo_transparente120.png" alt="logo" className="h-20"/>
+      <div className="flex flex-col md:flex-row justify-between items-center p-4">
+        <img src="https://islandpacific.com.mx/wp-content/uploads/2023/09/logo_transparente120.png"
+             alt="logo"
+             className="h-16 md:h-20"/>
         <ul className="flex">
-          <li className="mr-6"><a href="https://www.facebook.com/IslandPacific.Mx" target="_blank" rel="noopener noreferrer">
-                <IoLogoFacebook size={40} className="hover:text-blue-500" />
-          </a></li>
-          <li className="mr-6"><a href="https://www.instagram.com/islandpacific.mx/?hl=es-la" target="_blank" rel="noopener noreferrer">
-                <FaInstagram size={40} className="hover:text-pink-600"/>
-          </a></li>
-          <li><a href="https://www.google.com/maps/place/Island+Pacific+-+Buzos+y+Pescadores+de+la+Baja+California,+S.C.L./@31.8557625,-116.5896675,17z/data=!3m1!4b1!4m6!3m5!1s0x80d88d7b3a3355e5:0x88b3e708cc695f0a!8m2!3d31.8557625!4d-116.5896675!16s%2Fg%2F1tj2c8ml?authuser=0&entry=ttu" target="_blank" rel="noopener noreferrer">
-                <MdLocationPin size={40} className="hover:text-red-500" />
-          </a></li>
+          <li className="mr-6">
+            <a href="https://www.facebook.com/IslandPacific.Mx"
+               target="_blank"
+               rel="noopener noreferrer">
+              <IoLogoFacebook size={40} className="hover:text-blue-500" />
+            </a>
+          </li>
+          <li className="mr-6">
+            <a href="https://www.instagram.com/islandpacific.mx/?hl=es-la"
+               target="_blank"
+               rel="noopener noreferrer">
+              <FaInstagram size={40} className="hover:text-pink-600"/>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.google.com/maps/place/Island+Pacific+-+Buzos+y+Pescadores+de+la+Baja+California,+S.C.L./@31.8557625,-116.5896675,17z/data=!3m1!4b1!4m6!3m5!1s0x80d88d7b3a3355e5:0x88b3e708cc695f0a!8m2!3d31.8557625!4d-116.5896675!16s%2Fg%2F1tj2c8ml?authuser=0&entry=ttu"
+               target="_blank"
+               rel="noopener noreferrer">
+              <MdLocationPin size={40} className="hover:text-red-500" />
+            </a>
+          </li>
         </ul>
-        <p className="text-base">Derechos reservados © 2024 IslandPacific</p>
+        <p className="text-sm md:text-base">
+          Derechos reservados © 2024 IslandPacific
+        </p>
       </div>
     </div>
   );
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,37 +1,78 @@
 import Link from "next/link";
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebookF, faInstagram, faGooglePlus } from "@fortawesome/free-brands-svg-icons";
+import { IoLogoFacebook } from "react-icons/io";
+import { FaInstagram } from "react-icons/fa";
+import { MdLocationPin } from "react-icons/md";
 
 const Navigation = () => {
   return (
     <div className="bg-gray-800 text-white">
       <div className="w-full">
-        <img src="https://img.freepik.com/fotos-premium/marisco-mas-fresco-todos-gustos_181303-2861.jpg?w=900" alt="Seafood Header" className="w-full h-64 object-cover" />
+        <img
+          src="https://img.freepik.com/fotos-premium/marisco-mas-fresco-todos-gustos_181303-2861.jpg?w=900"
+          alt="Seafood Header"
+          className="w-full object-cover h-32 sm:h-48 md:h-64"
+        />
       </div>
-      <div className="flex justify-between items-center py-4 px-10">
-        <div className="flex items-center">
-          <img src="https://islandpacific.com.mx/wp-content/uploads/2023/09/cropped-logo4-768x335.png" alt="logo" className="h-12 w-auto mr-4"/>
-          <p>Buzos y pescadores de la Baja California</p>
+      <div className="flex flex-col sm:flex-row justify-between items-center py-4 px-10">
+        <div className="flex flex-col sm:flex-row items-center">
+          <img
+            src="https://islandpacific.com.mx/wp-content/uploads/2023/09/cropped-logo4-768x335.png"
+            alt="logo"
+            className="h-8 md:h-12 w-auto mr-4"
+          />
+          <p className="text-sm sm:text-md md:text-lg">
+            BUZOS Y PESCADORES DE LA BAJA CALIFORNIA
+          </p>
         </div>
-        <ul className="flex items-center">
+        <ul className="flex">
           <li className="mr-6">
-            <FontAwesomeIcon icon={faFacebookF} size="lg" className="hover:text-blue-500" />
+            <a
+              href="https://www.facebook.com/IslandPacific.Mx"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IoLogoFacebook size={40} className="hover:text-blue-500" />
+            </a>
           </li>
           <li className="mr-6">
-            <FontAwesomeIcon icon={faInstagram} size="lg" className="hover:text-purple-500" />
+            <a
+              href="https://www.instagram.com/islandpacific.mx/?hl=es-la"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram size={40} className="hover:text-pink-600" />
+            </a>
           </li>
           <li>
-            <FontAwesomeIcon icon={faGooglePlus} size="lg" className="hover:text-red-500" />
+            <a
+              href="https://www.google.com/maps/place/Island+Pacific+-+Buzos+y+Pescadores+de+la+Baja+California,+S.C.L./@31.8557625,-116.5896675,17z/data=!3m1!4b1!4m6!3m5!1s0x80d88d7b3a3355e5:0x88b3e708cc695f0a!8m2!3d31.8557625!4d-116.5896675!16s%2Fg%2F1tj2c8ml?authuser=0&entry=ttu"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <MdLocationPin size={40} className="hover:text-red-500" />
+            </a>
           </li>
         </ul>
       </div>
-      <div className="flex justify-center space-x-6 py-3 bg-blue-900">
-        <Link href="/">Inicio</Link>
-        <Link href="/page/products">Productos</Link>
-        <Link href="/page/installations">Instalaciones</Link>
-        <Link href="/page/request">Peticiones</Link>
-        <Link href="/page/cartContent">Carrito</Link>
+      <div className="flex overflow-x-auto py-3 bg-blue-900">
+        <div className="flex space-x-6 min-w-max">
+          <Link href="/" className="px-4 py-2">
+            Inicio
+          </Link>
+          <Link href="/page/products" className="px-4 py-2">
+            Productos
+          </Link>
+          <Link href="/page/installations" className="px-4 py-2">
+            Instalaciones
+          </Link>
+          <Link href="/page/request" className="px-4 py-2">
+            Peticiones
+          </Link>
+          <Link href="/page/cartContent" className="px-4 py-2">
+            Carrito
+          </Link>
+        </div>
       </div>
     </div>
   );
